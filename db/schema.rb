b5dffe64c59d01e1064872632996ee99cac2b300
Soojin Hong <soojinhong@Soojins-MacBook-Pro.local>
2020-06-24 15:46:10 +1000

    change to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_22_065040) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "animals", force: :cascade do |t|
     t.text "species"
     t.text "age"
